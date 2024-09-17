4886f18695a24f5fd0ec29a2d18214ceb3d60ee8
thomp2e3 <thomp2e3@mail.uc.edu>
2024-09-17 13:33:27 -0400

    Initial commit

---
+++ src/DoubleMyFun.java
@@ -0,0 +1,15 @@
+public class DoubleMyFun
+{
+    public static void main(String[] args)
+    {
+        int fun = 23;
+        int doubleFun = 0;
+
+
+        doubleFun = fun * 2;
+
+        System.out.println("Your fun (" + fun + ") doubled is " + doubleFun);
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        boolean done = true;
+        String name = "Ella";
+        int age = 19;
+
+
+        System.out.println("Hello " + name + "!");
+        System.out.println("You said your age was " + age + ".");
+        System.out.println(done);
+    }
+}
